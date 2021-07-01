d311ac283431aceba7228ca76157d28ef4d3511e
Alok Rai <74557370+alokrai@users.noreply.github.com>
2021-07-01 14:49:09 +0200

    updated event and user models

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,10 +2,9 @@ class User < ApplicationRecord
   has_secure_password
 
   has_many :registrations, dependent: :destroy
+  has_many :likes, dependent: :destroy
 
   validates :name, presence: true
   validates :email, format: URI::MailTo::EMAIL_REGEXP,
                    uniqueness: { case_sensitive: false }
-
-
 end
